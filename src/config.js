bf6adedcdb5d98dde7e7f7a1f9ed763364b0c35a
Cristian Gutierrez <yayobyte@gmail.com>
2020-04-05 21:11:36 +0400

    Revert "firebase api"
    This reverts commit 2011819148b8f1e3d1e0c0d7a65bd560c7c3a524.

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const API_KEY = "AIzaSyCPsRQ_3cfOEnyUHYEA2OpOvgUrWsKGvSw";
-const FIREBASE_AUTH_URL = "https://identitytoolkit.googleapis.com/v1/accounts:";
-export const SIGN_IN = 'signInWithPassword';
-export const SIGN_UP = 'signUp';
-
-export const getFirebaseUrl = (method) => (`${FIREBASE_AUTH_URL}${method}?key=${API_KEY}`);
